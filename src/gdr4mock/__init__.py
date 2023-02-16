a448aa7666d26fed91d20195c0b97ab365ee6ad0
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-02-16 13:13:14 +0100

    structure

--- src/gdr4mock/__init__.py
+++ src/gdr4mock/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("my_package")
+    __version__ = version("gdr4mock")
 except PackageNotFoundError:
     # package is not installed
     pass
